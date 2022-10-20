0fa11ab9342592615196c457e8e8eaea68713ea7
Vanessa Farias <vr.farias1992@gmail.com>
2022-10-19 21:32:03 -0300

    exercícios de Inputs Controlados

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -3,11 +3,13 @@ import {MainContainer} from './styles'
 import NameForm from '../NameForm/NameForm'
 import ConfirmationForm from '../ConfirmationForm/ConfirmationForm'
 
-const MainPage = () => {
+const MainPage = (props) => {
 const [name, setName] = useState("")
 const [age, setAge] = useState("")
 const [email, setEmail] = useState("")
 const [formFlow, setFormFlow] = useState(1) //você não precisa mexer neste estado, ele faz parte da lógica da linha 30 do JSX
+const [dataNascimento, setDataNascimento] = useState("")
+const [telefone, setTelefone] = useState("")
 
 const onChangeName = (event) => {
   setName(event.target.value)
@@ -21,16 +23,60 @@ const onChangeEmail = (event) => {
   setEmail(event.target.value)
 }
 
+const onChangeDataNascimento = (event) =>{
+  setDataNascimento(event.target.value)
+}
+
+const onChangeTelefone = (event) =>{
+  setTelefone(event.target.value)
+}
+
 const sendData = () => {
   //aqui deve vir uma verificação para mudar de formulario apenas se todos os requisitos tiverem sido cumpridos
-  setFormFlow(2)
+  if(name.length > 10 && name.length <= 30 && age >= 18 && email.includes("@") && email.includes(".com")){
+    alert("Próxima etapa!")
+    setFormFlow(2)
+  }else{
+    alert("Informações incorretas!")
+    setFormFlow(setFormFlow)
+  }
+  
 }
+
+const sendData2 = () => {
+  //aqui deve vir uma verificação para mudar de formulario apenas se todos os requisitos tiverem sido cumpridos
+  if(telefone.length === 13 && dataNascimento.length === age){
+    alert("Cadastro completo!")
+    setFormFlow(2)
+  }else{
+    alert("Informações incorretas!")
+    setFormFlow(setFormFlow)
+  }
+  
+}
+
   return (
     <MainContainer>
       <h2>Formulário de inscrição</h2>
       {formFlow === 1 ? <NameForm
       // insira aqui suas props
-      /> : <ConfirmationForm />}
+      nome={name}
+      idade={age}
+
+      email={email}
+      data={formFlow}
+
+      onChangeName={onChangeName}
+      onChangeAge={onChangeAge}
+      onChangeEmail={onChangeEmail}
+      sendData={sendData}
+      /> : <ConfirmationForm 
+      dataNascimento={dataNascimento}
+      telefone={telefone}
+      onChangeDataNascimento={onChangeDataNascimento}
+      onChangeTelefone={onChangeTelefone}
+      sendData2={sendData2}
+      />}
     </MainContainer>
   )
 }
--- src/Components/NameForm/NameForm.js
+++ src/Components/NameForm/NameForm.js
@@ -2,25 +2,26 @@ import React from 'react'
 import { Form, Input} from '../MainPage/styles'
 
 const NameForm = (props) => {
+
   return (
     <Form>
         <label>
           Nome:
-          <Input  placeholder="Nome" value={name} onChange={onChangeName}/>
+          <Input type='text' placeholder="Nome" value={props.nome} onChange={props.onChangeName}/>
         </label>
         <label>
           Idade:
-          <Input  placeholder="Idade" value={age} onChange={onChangeAge}/>
+          <Input type='number'  placeholder="Idade" value={props.idade} onChange={props.onChangeAge}/>
         </label>
         <label>
           E-mail:
-          <Input  placeholder="usuario@usuario.com" value={email} onChange={onChangeEmail}/>
+          <Input type='email'  placeholder="usuario@usuario.com" value={props.email} onChange={props.onChangeEmail}/>
         </label>
         <label>
           Confirmação de e-mail:
-          <Input  placeholder="usuario@usuario.com" value={email} onChange={onChangeEmail}/>
+          <Input type='email' placeholder="usuario@usuario.com" value={props.email} onChange={props.onChangeEmail}/>
         </label>
-      <button onClick={sendData}>Enviar dados</button>
+      <button onClick={props.sendData}>Enviar dados</button>
       </Form>
   )
 }
